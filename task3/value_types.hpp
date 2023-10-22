c877605ff43acc1fe192b10a601314cda0e2b1a0
BardRandall <egor-ulstu@yandex.ru>
2023-10-22 17:53:35 +0300

    Base solution

--- task3/value_types.hpp
+++ task3/value_types.hpp
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "type_tuples.hpp"
 
 namespace value_types
 {
@@ -8,6 +8,9 @@ template<auto V>
 struct ValueTag{ static constexpr auto Value = V; };
 
 template<class T, T... ts>
-using VTuple = TTuple<ValueTag<ts>...>;
+using VTuple = type_tuples::TTuple<ValueTag<ts>...>;
+
+template<class Tag, class T>
+concept VTag = requires(Tag t) { []<T x>(ValueTag<x>){}(t); };
 
 }
