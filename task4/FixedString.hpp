3976c3485e8ed65766f374feecb1cf0e7beb9cc3
BardRandall <egor-ulstu@yandex.ru>
2023-11-12 22:07:19 +0300

    Task 4 done

--- task4/FixedString.hpp
+++ task4/FixedString.hpp
@@ -1,12 +1,25 @@
 #pragma once
 
+#include <algorithm>
+#include <array>
+#include <string_view>
+
 
 template<size_t max_length>
 struct FixedString {
-  FixedString(const char* string, size_t length);
-  operator std::string_view() const;
+  constexpr FixedString(const char* string, size_t length) : storage({}), length(length) {
+    std::copy(string, string + length, storage.begin());
+    // std::fill(storage.begin() + length, storage.end(), '\0');
+  }
 
-  // std::string impl; ???
+  constexpr operator std::string_view() const {
+    return {storage.data(), length};
+  }
+
+  std::array<char, max_length> storage;
+  size_t length;
 };
 
-// operator ""_cstr ?
+constexpr FixedString<256> operator ""_cstr(const char* data, std::size_t length) {
+  return FixedString<256>(data, length);
+}
